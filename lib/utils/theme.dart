cd05622ba17955f66b4f558479b464bf9a18e836
Sudhansu Bhushan Mishra <sbmishra2000@gmail.com>
2020-06-21 13:39:33 +0530

    UI fixes

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -5,6 +5,7 @@ class Theming {
   static final lightTheme = ThemeData(
     primarySwatch: Colors.blue,
     primaryColor: AppColors.logoBlue,
+    accentColor: AppColors.white,
     fontFamily: 'HelveticaNeue',
     scaffoldBackgroundColor: Colors.white,
     backgroundColor: Colors.white,
@@ -14,19 +15,21 @@ class Theming {
       iconTheme: IconThemeData(
         color: AppColors.logoBlue,
       ),
+      centerTitle: true,
     ),
 
     buttonTheme: ButtonThemeData(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(18.0),
       ),
-      buttonColor: AppColors.logoBlue
+      buttonColor: AppColors.logoBlue,
+      textTheme: ButtonTextTheme.accent,
     ),
     visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 
   static final darkTheme = lightTheme.copyWith(
+    // TODO
     brightness: Brightness.dark,
-    primaryColor: AppColors.darkBlue,
   );
 }
